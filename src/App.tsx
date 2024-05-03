f39fe849841147e947f31c857d57a863c133b6fa
doaa51094 <112436865+doaa51094@users.noreply.github.com>
2024-05-03 15:10:00 +0300

    create:header and get all categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Home from "./components/home/Home";
+import "./index.css";
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element={<Home />} />
+      </Route>
+    )
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center">
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/components/home/Home.tsx
@@ -0,0 +1,11 @@
+import Header from '../header/Header'
+
+const Home = () => {
+  return (
+    <div className='mt-10 w-[80%] '>
+  <Header/>
+    </div>
+  )
+}
+
+export default Home
